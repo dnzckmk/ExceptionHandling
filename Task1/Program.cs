3304ba25052e3526d7953b24cf6c945a890285dd
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-05-18 12:46:07 +0300

    Add  a method that prints the first character of each entered input line and exception handling to validate input for empty string.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Task1
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+using System.Linq;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            Console.WriteLine("To close the app type 'exit'.");
+            string input;
+            do
+            {
+                Console.WriteLine("Enter text:");
+                input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    Environment.Exit(0);
+                }
+
+                try
+                {
+                    PrintTheFirstCharacter(input);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+            while (string.IsNullOrWhiteSpace(input) || input != "exit");
+
+            static void PrintTheFirstCharacter(string text)
+            {
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    throw new ArgumentNullException($"{nameof(text)} can't be empty.");
+                }
+
+                Console.WriteLine(text.First());
+            }
         }
     }
 }
