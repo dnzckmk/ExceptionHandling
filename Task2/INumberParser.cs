6d051a90f44547e2e8e83e62b909398e3aff4e5e
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-05-18 22:42:43 +0300

    Implement PArse method to convert string to integer without using built-in conversion methods.

--- Task2/INumberParser.cs
+++ Task2/INumberParser.cs
@@ -2,6 +2,11 @@
 {
     public interface INumberParser
     {
+        /// <summary>
+        /// Parse string value to integer value without using any library method.
+        /// </summary>
+        /// <param name="stringValue">String to be converted to integer.</param>
+        /// <returns>Returns integer value of the provided param.</returns>
         int Parse(string stringValue);
     }
 }
