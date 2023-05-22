a20762da3e3eb6686063ec2b9c908f7a3653a249
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-05-22 21:10:31 +0300

    Refactor UserTaskController and UserTaskService classes to change code-based error handling to exception-based one.

---
+++ Task3/Exceptions/InvalidUserIdException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task3.Exceptions
+{
+    /// <summary>
+    /// Invalid user id exception to throw when user id is not valid.
+    /// </summary>
+    public class InvalidUserIdException : Exception
+    {
+        public InvalidUserIdException()
+        {
+        }
+
+        public InvalidUserIdException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidUserIdException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected InvalidUserIdException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ Task3/Exceptions/TaskAlreadyExistsException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task3.Exceptions
+{
+    /// <summary>
+    /// Task already exists exception to throw when the UserTask already exists.
+    /// </summary>
+    public class TaskAlreadyExistsException : Exception
+    {
+        public TaskAlreadyExistsException()
+        {
+        }
+
+        public TaskAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public TaskAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TaskAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
